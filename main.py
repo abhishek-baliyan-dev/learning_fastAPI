7cead984f58a21c189fcb22031beee17e29ac09b
Abhishek Baliyan <abhishek.baliyan.dev@gmail.com>
2024-08-16 21:01:24 +0530

    Best Practices - HTTP response
    - Change the status code 200 to 201 for creating post.

--- main.py
+++ main.py
@@ -29,15 +29,13 @@ async def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    # print(post)
-    # print(post.model_dump())
     post_dict = post.model_dump()
     post_dict["id"] = randrange(0, 1000000)
     my_posts.append(post_dict)
-    return {"data": post}
-# title str, content str
+    return {"data": post_dict}
+
 
 @app.get("/posts/{id}")
 def get_post(id: int):
